2d08f4b12635f6dab06a43bd64f5e2bdedc60bbb
BearTheCoder <58434823+BearTheCoder@users.noreply.github.com>
2022-05-29 19:53:37 -0400

    New Changes

--- JumpKingMacro/User32.cs
+++ JumpKingMacro/User32.cs
@@ -12,5 +12,11 @@ namespace JumpKingMacro
         internal static extern short GetAsyncKeyState(int vKey); //Detects keyboard input.
         [DllImport("user32.dll")]
         internal static extern short SendInput(int vKey); //Detects keyboard input.
+        [DllImport("user32.dll")]
+        internal static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo); //Triggers mouse.
+        [DllImport("user32.dll")]
+        internal static extern bool SetCursorPos(int x, int y); // (0,0) = Top-Left; (1920, 1080) = Bottom-Right
+        [DllImport("user32.dll")]
+        internal static extern bool GetCursorPos(out Point P); // (0,0) = Top-Left; (1920, 1080) = Bottom-Right
     }
 }
