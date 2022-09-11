596358d9d6fbe2bec75734fc1848fede3778b80b
BearTheCoder <58434823+BearTheCoder@users.noreply.github.com>
2022-09-11 16:50:36 -0400

    Upload File

---
+++ Twitter and Amazon/Code/SensitiveInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterTest2
+{
+    internal class SensitiveInfo
+    {
+        internal static string EMAIL = "";
+        internal static string PASSWORD = "";
+        internal static string EMAILPASS = "";
+        internal static string API_Key = "";
+        internal static string API_Secret = "";
+        internal static string Access_Token = "";
+        internal static string Access_Secret = "";
+    }
+}
